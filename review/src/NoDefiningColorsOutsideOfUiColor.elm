4b0b549c32d4b3b0672bd597a2e5cc5805631935
Jeroen Engels <jfm.engels@gmail.com>
2020-04-08 00:59:24 +0200

    WIP adapt rules to v2 API

--- review/src/NoDefiningColorsOutsideOfUiColor.elm
+++ review/src/NoDefiningColorsOutsideOfUiColor.elm
@@ -29,50 +29,30 @@ type Context
 
 rule : Rule
 rule =
-    Rule.newSchema "NoDefiningColorsOutsideOfUiColor"
-        |> Rule.withInitialContext ForbiddenToDefineColors
-        |> Rule.withModuleDefinitionVisitor moduleDefinitionVisitor
+    Rule.newModuleRuleSchema "NoDefiningColorsOutsideOfUiColor" ()
         |> Rule.withExpressionVisitor expressionVisitor
-        |> Rule.fromSchema
+        |> Rule.fromModuleRuleSchema
+        |> Rule.ignoreErrorsForModules [ "Ui.Color" ]
 
 
-moduleDefinitionVisitor : Node Module -> context -> ( List Error, Context )
-moduleDefinitionVisitor (Node range moduleDefinition) context =
-    case Module.moduleName moduleDefinition of
-        -- If the analyzed file is Ui.Color, then we don't want to report anything.
-        -- This is the sole location where we are allowed to defined colors.
-        [ "Ui", "Color" ] ->
-            ( [], AllowedToDefineColors )
-
-        _ ->
-            ( [], ForbiddenToDefineColors )
-
-
-expressionVisitor : Node Expression -> Direction -> Context -> ( List Error, Context )
-expressionVisitor node direction context =
-    case context of
-        AllowedToDefineColors ->
-            ( [], context )
-
-        ForbiddenToDefineColors ->
-            case ( direction, Node.value node ) of
-                ( Rule.OnEnter, Expression.Application (function :: arguments) ) ->
-                    case Node.value function of
-                        Expression.FunctionOrValue [ "Css" ] "hex" ->
-                            ( [ Rule.error
-                                    { message = "Do not define colors outside of Ui.Color"
-                                    , details =
-                                        [ "At fruits.com, we try to have all the colors in our application defined in the Ui.Color file. This helps us to have a consistent color palette across the application."
-                                        , "You should define this color in the Ui.Color module, and import it to use it at this location. Do check whether the color does not already exist though."
-                                        ]
-                                    }
-                                    (Node.range node)
-                              ]
-                            , context
-                            )
-
-                        _ ->
-                            ( [], context )
+expressionVisitor : Node Expression -> List (Error {})
+expressionVisitor node =
+    case Node.value node of
+        Expression.Application (function :: arguments) ->
+            case Node.value function of
+                Expression.FunctionOrValue [ "Css" ] "hex" ->
+                    [ Rule.error
+                        { message = "Do not define colors outside of Ui.Color"
+                        , details =
+                            [ "At fruits.com, we try to have all the colors in our application defined in the Ui.Color file. This helps us to have a consistent color palette across the application."
+                            , "You should define this color in the Ui.Color module, and import it to use it at this location. Do check whether the color does not already exist though."
+                            ]
+                        }
+                        (Node.range node)
+                    ]
 
                 _ ->
-                    ( [], context )
+                    []
+
+        _ ->
+            []
