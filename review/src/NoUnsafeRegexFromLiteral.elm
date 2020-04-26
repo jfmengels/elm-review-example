78eabbbce4356802d536ae304e762ffa56881ea0
Jeroen Engels <jfm.engels@gmail.com>
2020-04-26 19:47:38 +0200

    Report when the target function could not be found

--- review/src/NoUnsafeRegexFromLiteral.elm
+++ review/src/NoUnsafeRegexFromLiteral.elm
@@ -2,11 +2,11 @@ module NoUnsafeRegexFromLiteral exposing (rule)
 
 {-| Forbids misusing the unsafe function `Helpers.Regex.fromLiteral`.
 
-**Note**: This version is too simplistic to be used. Check out the `master` branch
-for the final and safe version.
+@docs rule
 
 -}
 
+import Elm.Syntax.Declaration as Declaration exposing (Declaration)
 import Elm.Syntax.Exposing as Exposing
 import Elm.Syntax.Expression as Expression exposing (Expression)
 import Elm.Syntax.Import exposing (Import)
@@ -20,36 +20,24 @@ import Scope
 
 rule : Rule
 rule =
-    Rule.newModuleRuleSchema "NoUnsafeRegexFromLiteral" initialContext
-        |> Scope.addModuleVisitors
-        |> Rule.withImportVisitor importVisitor
+    Rule.newProjectRuleSchema "NoUnsafeRegexFromLiteral" initialProjectContext
+        |> Scope.addProjectVisitors
+        |> Rule.withElmJsonProjectVisitor elmJsonVisitor
+        |> Rule.withModuleVisitor moduleVisitor
+        |> Rule.withModuleContext
+            { fromProjectToModule = fromProjectToModule
+            , fromModuleToProject = fromModuleToProject
+            , foldProjectContexts = foldProjectContexts
+            }
+        |> Rule.withFinalProjectEvaluation finalProjectEvaluation
+        |> Rule.fromProjectRuleSchema
+
+
+moduleVisitor : Rule.ModuleRuleSchema {} ModuleContext -> Rule.ModuleRuleSchema { hasAtLeastOneVisitor : () } ModuleContext
+moduleVisitor schema =
+    schema
+        |> Rule.withDeclarationListVisitor declarationListVisitor
         |> Rule.withExpressionVisitor expressionVisitor
-        |> Rule.fromModuleRuleSchema
-
-
-type alias Context =
-    { scope : Scope.ModuleContext
-    , fromLiteralWasExposed : Bool
-    , allowedFunctionOrValues : List Range
-    }
-
-
-initialContext : Context
-initialContext =
-    { scope = Scope.initialModuleContext
-    , fromLiteralWasExposed = False
-    , allowedFunctionOrValues = []
-    }
-
-
-isTargetFunction : Context -> ModuleName -> String -> Bool
-isTargetFunction context moduleName functionName =
-    if functionName /= targetFunctionName then
-        False
-
-    else
-        (Scope.realModuleName context.scope targetFunctionName moduleName == targetModuleName)
-            || (List.isEmpty moduleName && context.fromLiteralWasExposed)
 
 
 targetModuleName : List String
@@ -62,27 +50,122 @@ targetFunctionName =
     "fromLiteral"
 
 
-importVisitor : Node Import -> Context -> ( List nothing, Context )
-importVisitor (Node.Node _ { moduleName, exposingList }) context =
-    if Node.value moduleName == targetModuleName then
-        case Maybe.map Node.value exposingList of
-            Just (Exposing.All _) ->
-                ( [], { context | fromLiteralWasExposed = True } )
+type alias ProjectContext =
+    { scope : Scope.ProjectContext
+    , elmJsonKey : Maybe Rule.ElmJsonKey
+    , foundTargetFunction : Bool
+    }
 
-            _ ->
-                ( [], context )
+
+type alias ModuleContext =
+    { scope : Scope.ModuleContext
+    , allowedFunctionOrValues : List Range
+    , foundTargetFunction : Bool
+    }
+
+
+initialProjectContext : ProjectContext
+initialProjectContext =
+    { scope = Scope.initialProjectContext
+    , elmJsonKey = Nothing
+    , foundTargetFunction = False
+    }
+
+
+fromProjectToModule : Rule.ModuleKey -> Node ModuleName -> ProjectContext -> ModuleContext
+fromProjectToModule _ _ projectContext =
+    { scope = Scope.fromProjectToModule projectContext.scope
+    , allowedFunctionOrValues = []
+    , foundTargetFunction = False
+    }
+
+
+fromModuleToProject : Rule.ModuleKey -> Node ModuleName -> ModuleContext -> ProjectContext
+fromModuleToProject _ moduleNameNode moduleContext =
+    { scope = Scope.fromModuleToProject moduleNameNode moduleContext.scope
+    , elmJsonKey = Nothing
+    , foundTargetFunction = moduleContext.foundTargetFunction && (Node.value moduleNameNode == targetModuleName)
+    }
+
+
+foldProjectContexts : ProjectContext -> ProjectContext -> ProjectContext
+foldProjectContexts newContext previousContext =
+    { scope = Scope.foldProjectContexts newContext.scope previousContext.scope
+    , elmJsonKey = previousContext.elmJsonKey
+    , foundTargetFunction = previousContext.foundTargetFunction || newContext.foundTargetFunction
+    }
+
+
+elmJsonVisitor : Maybe { a | elmJsonKey : Rule.ElmJsonKey } -> ProjectContext -> ( List nothing, ProjectContext )
+elmJsonVisitor elmJson projectContext =
+    ( [], { projectContext | elmJsonKey = Maybe.map .elmJsonKey elmJson } )
+
+
+finalProjectEvaluation : ProjectContext -> List (Error scope)
+finalProjectEvaluation projectContext =
+    if projectContext.foundTargetFunction then
+        []
 
     else
-        ( [], context )
+        case projectContext.elmJsonKey of
+            Just elmJsonKey ->
+                [ Rule.errorForElmJson
+                    elmJsonKey
+                    (\_ ->
+                        { message = "Could not find Helpers.Regex.fromLiteral."
+                        , details =
+                            [ "I want to provide guarantees on the use of this function, but I can't find it. It is likely that it was renamed, which prevents me from giving you these guarantees."
+                            , "You should rename it back or update this rule to the new name. If you do not use the function anymore, remove the rule."
+                            ]
+                        , range =
+                            { start = { row = 1, column = 1 }
+                            , end = { row = 1, column = 2 }
+                            }
+                        }
+                    )
+                ]
+
+            Nothing ->
+                []
 
 
-expressionVisitor : Node Expression -> Rule.Direction -> Context -> ( List (Error {}), Context )
-expressionVisitor node direction context =
+declarationListVisitor : List (Node Declaration) -> ModuleContext -> ( List nothing, ModuleContext )
+declarationListVisitor nodes moduleContext =
+    let
+        foundTargetFunction : Bool
+        foundTargetFunction =
+            List.any
+                (\node ->
+                    case Node.value node of
+                        Declaration.FunctionDeclaration function ->
+                            targetFunctionName
+                                == (function.declaration
+                                        |> Node.value
+                                        |> .name
+                                        |> Node.value
+                                   )
+
+                        _ ->
+                            False
+                )
+                nodes
+    in
+    ( [], { moduleContext | foundTargetFunction = foundTargetFunction } )
+
+
+isTargetFunction : ModuleContext -> ModuleName -> String -> Bool
+isTargetFunction moduleContext moduleName functionName =
+    (functionName == targetFunctionName)
+        && (Scope.realModuleName moduleContext.scope targetFunctionName moduleName == targetModuleName)
+
+
+expressionVisitor : Node Expression -> Rule.Direction -> ModuleContext -> ( List (Error {}), ModuleContext )
+expressionVisitor node direction moduleContext =
     case ( direction, Node.value node ) of
         ( Rule.OnEnter, Expression.Application (function :: argument :: []) ) ->
             case Node.value function of
                 Expression.FunctionOrValue moduleName functionName ->
-                    if isTargetFunction context moduleName functionName then
+                    if isTargetFunction moduleContext moduleName functionName then
                         let
                             errors : List (Error {})
                             errors =
@@ -96,49 +179,49 @@ expressionVisitor node direction context =
                                                 [ Rule.error invalidRegex (Node.range node) ]
 
                                     _ ->
-                                        [ Rule.error nonLiteralValue (Node.range node) ]
+                                        [ Rule.error nonStaticValue (Node.range node) ]
                         in
                         ( errors
-                        , { context | allowedFunctionOrValues = Node.range function :: context.allowedFunctionOrValues }
+                        , { moduleContext | allowedFunctionOrValues = Node.range function :: moduleContext.allowedFunctionOrValues }
                         )
 
                     else
-                        ( [], context )
+                        ( [], moduleContext )
 
                 _ ->
-                    ( [], context )
+                    ( [], moduleContext )
 
         ( Rule.OnEnter, Expression.FunctionOrValue moduleName functionName ) ->
             if
-                isTargetFunction context moduleName functionName
-                    && not (List.member (Node.range node) context.allowedFunctionOrValues)
+                isTargetFunction moduleContext moduleName functionName
+                    && not (List.member (Node.range node) moduleContext.allowedFunctionOrValues)
             then
                 ( [ Rule.error notUsedAsFunction (Node.range node) ]
-                , context
+                , moduleContext
                 )
 
             else
-                ( [], context )
+                ( [], moduleContext )
 
         _ ->
-            ( [], context )
+            ( [], moduleContext )
 
 
 invalidRegex : { message : String, details : List String }
 invalidRegex =
     { message = "Helpers.Regex.fromLiteral needs to be called with a valid regex."
     , details =
-        [ "The regex you passed does not evaluate to a valid regex. Please fix it or use `Regex.fromString`."
+        [ "This function serves to give you more guarantees about creating regular expressions, but if the argument is dynamic or too complex, I won't be able to tell you."
         ]
     }
 
 
-nonLiteralValue : { message : String, details : List String }
-nonLiteralValue =
+nonStaticValue : { message : String, details : List String }
+nonStaticValue =
     { message = "Helpers.Regex.fromLiteral needs to be called with a static string literal."
     , details =
         [ "This function serves to give you more guarantees about creating regular expressions, but if the argument is dynamic or too complex, I won't be able to tell you."
-        , "Either make the argument static or use Regex.fromString."
+        , "Either make the argument static or use Regex.fromString instead."
         ]
     }
 
--- review/tests/NoUnsafeRegexFromLiteralTest.elm
+++ review/tests/NoUnsafeRegexFromLiteralTest.elm
@@ -1,8 +1,26 @@
 module NoUnsafeRegexFromLiteralTest exposing (all)
 
+import Elm.Project
+import Expect exposing (Expectation)
+import Json.Decode as Decode
 import NoUnsafeRegexFromLiteral exposing (rule)
+import Review.Project as Project exposing (Project)
 import Review.Test
 import Test exposing (Test, describe, test)
+
+
+helpersRegexSourceCode : String
+helpersRegexSourceCode =
+    """module Helpers.Regex exposing (fromLiteral)
+import Regex exposing (Regex)
+fromLiteral : String -> Regex
+fromLiteral = something
+"""
+
+
+expectErrors : List Review.Test.ExpectedError -> Review.Test.ReviewResult -> Expectation
+expectErrors expectedErrors =
+    Review.Test.expectErrorsForModules [ ( "A", expectedErrors ) ]
 
 
 all : Test
@@ -10,112 +28,120 @@ all =
     describe "NoUnsafeRegexFromLiteral"
         [ test "should not report calls to Helpers.Regex.fromLiteral with a valid literal regex" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex
 a = Helpers.Regex.fromLiteral "^abc$"
-"""
-                    |> Review.Test.run rule
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
                     |> Review.Test.expectNoErrors
         , test "should report calls to Helpers.Regex.fromLiteral with an invalid literal regex" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex
 a = Helpers.Regex.fromLiteral "^ab($cd"
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
+                    |> expectErrors
                         [ Review.Test.error
                             { message = "Helpers.Regex.fromLiteral needs to be called with a valid regex."
                             , details =
-                                [ "The regex you passed does not evaluate to a valid regex. Please fix it or use `Regex.fromString`."
+                                [ "This function serves to give you more guarantees about creating regular expressions, but if the argument is dynamic or too complex, I won't be able to tell you."
                                 ]
                             , under = """Helpers.Regex.fromLiteral "^ab($cd\""""
                             }
                         ]
+        , test "should not report calls to Helpers.Regex.fromLiteral with an valid literal regex containing back-slashes" <|
+            \_ ->
+                [ """module A exposing (..)
+import Helpers.Regex
+a = Helpers.Regex.fromLiteral "\\\\s"
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
+                    |> Review.Test.expectNoErrors
         , test "should report calls to Helpers.Regex.fromLiteral with a non-literal value" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex
 a = Helpers.Regex.fromLiteral dynamicValue
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
+                    |> expectErrors
                         [ Review.Test.error
                             { message = "Helpers.Regex.fromLiteral needs to be called with a static string literal."
                             , details =
                                 [ "This function serves to give you more guarantees about creating regular expressions, but if the argument is dynamic or too complex, I won't be able to tell you."
-                                , "Either make the argument static or use Regex.fromString."
+                                , "Either make the argument static or use Regex.fromString instead."
                                 ]
                             , under = "Helpers.Regex.fromLiteral dynamicValue"
                             }
                         ]
         , test "should report invalid calls if the function is called through a module alias" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex as R
 a = R.fromLiteral dynamicValue
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
+                    |> expectErrors
                         [ Review.Test.error
                             { message = "Helpers.Regex.fromLiteral needs to be called with a static string literal."
                             , details =
                                 [ "This function serves to give you more guarantees about creating regular expressions, but if the argument is dynamic or too complex, I won't be able to tell you."
-                                , "Either make the argument static or use Regex.fromString."
+                                , "Either make the argument static or use Regex.fromString instead."
                                 ]
                             , under = "R.fromLiteral dynamicValue"
                             }
                         ]
         , test "should report invalid calls if the function is called through a direct import" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex exposing (fromLiteral)
 a = fromLiteral dynamicValue
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
+                    |> expectErrors
                         [ Review.Test.error
                             { message = "Helpers.Regex.fromLiteral needs to be called with a static string literal."
                             , details =
                                 [ "This function serves to give you more guarantees about creating regular expressions, but if the argument is dynamic or too complex, I won't be able to tell you."
-                                , "Either make the argument static or use Regex.fromString."
+                                , "Either make the argument static or use Regex.fromString instead."
                                 ]
                             , under = "fromLiteral dynamicValue"
                             }
                         ]
         , test "should report invalid calls if the function is called through an import that exposes all" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex exposing (..)
 a = fromLiteral dynamicValue
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
+                    |> expectErrors
                         [ Review.Test.error
                             { message = "Helpers.Regex.fromLiteral needs to be called with a static string literal."
                             , details =
                                 [ "This function serves to give you more guarantees about creating regular expressions, but if the argument is dynamic or too complex, I won't be able to tell you."
-                                , "Either make the argument static or use Regex.fromString."
+                                , "Either make the argument static or use Regex.fromString instead."
                                 ]
                             , under = "fromLiteral dynamicValue"
                             }
                         ]
         , test "should not report invalid calls if the function was not imported" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex
 a = fromLiteral dynamicValue
-"""
-                    |> Review.Test.run rule
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
                     |> Review.Test.expectNoErrors
         , test "should report when function is used is used in a non 'function call' context" <|
             \_ ->
-                """module A exposing (..)
+                [ """module A exposing (..)
 import Helpers.Regex
 fromLiteralAlias = Helpers.Regex.fromLiteral
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
+""", helpersRegexSourceCode ]
+                    |> Review.Test.runOnModulesWithProjectData project rule
+                    |> expectErrors
                         [ Review.Test.error
                             { message = "Helpers.Regex.fromLiteral must be called directly."
                             , details =
@@ -124,4 +150,66 @@ fromLiteralAlias = Helpers.Regex.fromLiteral
                             , under = "Helpers.Regex.fromLiteral"
                             }
                         ]
+        , test "should report a global error when the target function could not be found in the project" <|
+            \_ ->
+                """module A exposing (..)
+import Helpers.Regex
+a = Helpers.Regex.fromLiteral "^abc$"
+"""
+                    |> Review.Test.runWithProjectData project rule
+                    |> Review.Test.expectErrorsForElmJson
+                        [ Review.Test.error
+                            { message = "Could not find Helpers.Regex.fromLiteral."
+                            , details =
+                                [ "I want to provide guarantees on the use of this function, but I can't find it. It is likely that it was renamed, which prevents me from giving you these guarantees."
+                                , "You should rename it back or update this rule to the new name. If you do not use the function anymore, remove the rule."
+                                ]
+                            , under = "{"
+                            }
+                            |> Review.Test.atExactly { start = { row = 1, column = 1 }, end = { row = 1, column = 2 } }
+                        ]
         ]
+
+
+
+-- PROJECT DATA
+
+
+project : Project
+project =
+    Project.new
+        |> Project.addElmJson (createElmJson applicationElmJson)
+
+
+createElmJson : String -> { path : String, raw : String, project : Elm.Project.Project }
+createElmJson rawElmJson =
+    case Decode.decodeString Elm.Project.decoder rawElmJson of
+        Ok elmJson ->
+            { path = "elm.json"
+            , raw = rawElmJson
+            , project = elmJson
+            }
+
+        Err _ ->
+            Debug.todo "Invalid elm.json supplied to test"
+
+
+applicationElmJson : String
+applicationElmJson =
+    """{
+    "type": "application",
+    "source-directories": [
+        "src"
+    ],
+    "elm-version": "0.19.1",
+    "dependencies": {
+        "direct": {
+            "elm/core": "1.0.0"
+        },
+        "indirect": {}
+    },
+    "test-dependencies": {
+        "direct": {},
+        "indirect": {}
+    }
+}"""
